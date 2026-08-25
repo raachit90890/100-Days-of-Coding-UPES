f2bd73961c61f95827c8d2057dfa4fb40ddc92ea
Raachit Deshmukh <137514760+raachit90890@users.noreply.github.com>
2026-08-25 20:36:16 +0530

    Add files via upload

---
+++ Day01/Q002.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int a,b;
+printf("write the numbers you want to perform operations on");
+scanf("%d%d",&a,&b);
+printf("the sum of the numbers is %d\n", a+b);
+printf("the product of the numbers is %d\n",a*b);
+printf("the difference of the numbers is %d\n",a-b);
+printf("the quoitent is %d",a%b);
+return 0;
+}
